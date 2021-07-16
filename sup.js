21e114404ccd151239436e884f03c7c138b84626
Ray Louis <x1337c@gmail.com>
2021-07-15 23:37:27 -0700

    Update sup.js

--- sup.js
+++ sup.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app = express();
+// app.set('trust proxy', true);
+app.use(express.json())
+
+app.use(function(request, response, next) {
+  console.log("got request", request.url);
+  next();
+})
+
+app.get("/", function(req, res) {
+  res.send("landing page")
+})
+
+app.use(function(request, response) {
+  response.status(404);
+  response.send("Cannot answer this request");
+})
+
+// listen for requests :)
+const listener = app.listen(3000, () => {
+  console.log("The static server is listening on port " + listener.address().port);
+});
